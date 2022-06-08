c5937b9e10efd47664cd2e9cb9b2793ce33fec53
Kjaroszew <klaudia.jaroszewicz@saleor.io>
2022-06-08 11:41:11 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/abandoned-checkouts.tsx
@@ -0,0 +1,50 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import { NextPage } from "next";
+import { useFetchAllCheckoutsQuery } from "../generated/graphql";
+
+const AbandonedCheckoutsPage: NextPage = () => {
+  const [{ data, error }] = useFetchAllCheckoutsQuery();
+
+  if (!data?.checkouts) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Abandoned Checkouts</h1>
+
+      <main>
+        {data?.checkouts.edges.length > 0 ? (
+          <TableContainer component={Paper}>
+            <Table aria-label="checkouts table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>No.</TableCell>
+                  <TableCell>Checkout Id</TableCell>
+                  <TableCell>Created At</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.checkouts.edges.map((row, i) => (
+                  <TableRow key={row.node.id}>
+                    <TableCell>{i + 1}.</TableCell>
+                    <TableCell>{row.node.id}</TableCell>
+                    <TableCell>{row.node.created}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <div>No data to display...</div>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default AbandonedCheckoutsPage;
